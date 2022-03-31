ccebe56df3482f6937b2aed1782d872082b94560
Kiru <mail@kiru.gay>
2022-04-01 00:10:52 +0200

    recursive hamination, and more ham images
    best title to a commit ive ever written

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+
 let images = fs.readdirSync(__dirname + "/images");
 
 function between(max, min) {
@@ -21,19 +22,31 @@ function getName() {
 	if (random()) {str = str.replaceAll("", "_")}
 
 	return str;
-}; console.log(getName())
+}
 
 function getImage() {
 	let image = images[Math.floor(Math.random() * images.length)];
 	return {
 		filename: image,
 		path: __dirname + "/images/" + image,
+		extension: image.replace(/.*\./g, ""),
 	}
 }
 
 function haminate(dir) {
+	let files = fs.readdirSync(dir);
+	for (let i = 0; i < files.length; i++) {
+		let file  = dir + "/" + files[i];
+		if (fs.statSync(file).isDirectory()) {
+			haminate(file);
+		}
+	}
+
 	fs.mkdirSync(dir, {recursive: true});
 
-	let img = getImage();
-	fs.copyFileSync(img.path, path.join(dir,  between(99999999999999, 999999) + getName() + img.filename))
+	let aggressiveness = between(50, 150);
+	for (let i = 0; i < aggressiveness; i++) {
+		let img = getImage();
+		fs.copyFileSync(img.path, path.join(dir,  getName() + between(99999999999999, 999999) + getName() + "." + img.extension))
+	}
 }; haminate("example-folder")
