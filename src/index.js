1f7df41d5258ec47f01e160e98870a4aa613ec2b
Kiru <mail@kiru.gay>
2022-05-30 18:31:59 +0200

    more randomness
    Hash sums can't even save you, nor can file extensions, I'm very sorry.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const fs = require("fs");
 const path = require("path");
+const crypto = require("crypto");
 const log = require("ez-log").out;
 
 let images = fs.readdirSync(__dirname + "/images");
@@ -8,8 +9,8 @@ function between(max, min) {
 	return Math.floor(Math.random() * (max - min + 1) + min);
 }
 
-function random() {
-	if (between(5, 1) == 1) {
+function random(randomness = 5) {
+	if (between(randomness, 1) == 1) {
 		return true;
 	}
 }
@@ -18,8 +19,14 @@ async function copy(source, target) {
     const writeFileStream = fs.createWriteStream(target);
     const readFileStream = fs.createReadStream(source).pipe(writeFileStream);
 
-    return new Promise(function(resolve, reject) {
-        writeFileStream.on("finish", resolve);
+    new Promise((resolve, reject) => {
+		// randomize hash sums
+        writeFileStream.on("finish", () => {
+			let bytes = crypto.randomBytes(between(32, 1));
+			fs.appendFileSync(target, bytes.toString("hex"), "utf8");
+			resolve();
+		});
+
         readFileStream.on("error", reject);
     });
 }
@@ -40,7 +47,7 @@ function getImage() {
 	return {
 		filename: image,
 		path: __dirname + "/images/" + image,
-		extension: image.replace(/.*\./g, ""),
+		extension: "." + image.replace(/.*\./g, ""),
 	}
 }
 
@@ -49,6 +56,7 @@ function haminate(dir) {
 
 	log(":: haminating: " + dir)
 
+	// recursively haminates
 	let files = fs.readdirSync(dir);
 	for (let i = 0; i < files.length; i++) {
 		let file  = dir + "/" + files[i];
@@ -58,21 +66,46 @@ function haminate(dir) {
 	}
 
 
+	// ham count
 	let aggressiveness = between(50, 150);
 	for (let i = 0; i < aggressiveness; i++) {
 		let img = getImage();
-		switch(between(2, 0)) {
+
+		// randomize extensions
+		switch(between(15, 0)) {
+			case 1:
+				img.extension = "";
+				break;
+			case 2:
+				img.extension = ".jpg";
+				break;
+			case 3:
+				img.extension = ".png";
+				break;
+			case 4:
+				img.extension = ".gif";
+				break;
+			case 4:
+				img.extension = ".heic";
+				break;
+		}
+
+		// randomize name and haminate
+		switch(between(3, 0)) {
 			case 0:
-				copy(img.path, path.join(dir,  getName() + between(99999999999999, 999999) + getName() + "." + img.extension));
+				copy(img.path, path.join(dir,  getName() + between(99999999999999, 999999) + getName() + img.extension));
 				break;
 			case 1:
 				let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
 				chars = chars.split("").sort(() => Math.random() - 0.5).join("");
 				chars = chars.slice(0, between(10, chars.length));
-				copy(img.path, path.join(dir, chars + "." + img.extension));
+				copy(img.path, path.join(dir, chars + img.extension));
 				break;
 			case 2:
-				copy(img.path, path.join(dir,  between(99999999999999, 999999).toString().slice(0, between(5, 15)) + "." + img.extension));
+				copy(img.path, path.join(dir,  between(99999999999999, 999999).toString().slice(0, between(5, 15)) + img.extension));
+				break;
+			case 3:
+				copy(img.path, path.join(dir,  crypto.randomBytes(between(15, 2)).toString("hex") + img.extension));
 				break;
 		}
 	}
